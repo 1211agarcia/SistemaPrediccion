a8da85f6f64a4f4f10731ea46db543d31c9e3c20
Armando <1211agarcia@gmail.com>
2016-04-26 07:02:29 -0430

    include fosJsRouting

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new PrediccionBundle\PrediccionBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
