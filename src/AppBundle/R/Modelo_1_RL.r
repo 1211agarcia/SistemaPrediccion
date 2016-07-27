9b26d792f98aae26364a78f24f683b15c6cd7080
Armando <1211agarcia@gmail.com>
2016-07-27 07:19:47 -0400

    nuestra nueva

--- src/AppBundle/R/Modelo_1_RL.r
+++ src/AppBundle/R/Modelo_1_RL.r
@@ -2,7 +2,7 @@
 
 
 #se cargan los datos colocando como parametros que los decimales seran separados por un ","
-data <- read.table("muestra_real.txt",header=TRUE)
+data <- read.table("muestra_estudiantes.txt",header=TRUE)
 data
 #Nombre de columnas a ignorar para este experimento, calclulo de ignora pues es la salida deseada
 drops <- c("cant_mat","escuela","gestion_plantel","tipo_plantel","nivel_socioeco","nivel_estudios_padres","genero","opsu")
